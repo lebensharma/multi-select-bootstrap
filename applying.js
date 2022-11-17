797443df514fb7f932451fc068ed66dbc5f5df25
lebensharma <aashu.0704@zohomail.in>
2022-11-17 11:11:12 +0530

    Removed multiSelect() in applying js and added it in index.js

--- applying.js
+++ applying.js
@@ -1,12 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const CreateMultiSelectElement = async () => {
-        await DropdownMultiSelect(placeholder, jsonFilePath, idOrNameToBeSuppliedToInput);
-    }
     const placeholder = "Please click to open the dropdown. You can Ctrl+Click to select multipe branch names.";
     const jsonFilePath = "static.json";
     const idOrNameToBeSuppliedToInput = "branches";
-    CreateMultiSelectElement()
-        .then(() => {
-            document.querySelector('select').multipleSelect();
-        })
+    DropdownMultiSelect(placeholder, jsonFilePath, idOrNameToBeSuppliedToInput);
 });
